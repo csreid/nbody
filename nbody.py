c7b59a85e40d280865e8035180447e1a97d5adc7
Cameron Reid <cameronreid@Cameron-Reids-iMac.local>
2014-04-26 14:37:44 -0400

    moved to pygame for images. sorta

--- nbody.py
+++ nbody.py
@@ -2,17 +2,19 @@ import math
 import time
 import Tkinter
 import random
+import pygame
 from Tkinter import Tk, Canvas, PhotoImage, mainloop
 
-WIDTH, HEIGHT = 1920, 1024
+size = WIDTH, HEIGHT = 640, 480
+black = 0,0,0
+white = 255,255,255
+#window = Tk()
 
-window = Tk()
-canvas = Canvas(window, width = WIDTH, height = HEIGHT, bg="#ffffff")
-canvas.pack()
-img = PhotoImage(width = WIDTH, height = HEIGHT)
-canvas.create_image((WIDTH/2,HEIGHT/2), image=img, state="normal")
+pygame.init()
+window = pygame.display.set_mode(size)
+window.fill(white)
 
-G = 6.67
+G = .667
 timestep = 0
 timestepsize = 1.0 #higher timesteps result in less accuracy but faster speeds. I think.
 bodies = []
@@ -54,78 +56,58 @@ def angle(a, b):
 def roundTuple((x, y)):
     return ((int)(round(x)), (int)(round(y)))
 
-def paint_at((x, y), n):
-    if n == 0 and x > 2 and y > 2:
-        img.put("#ffffff", (x, y))
-        img.put("#ffffff", (x+1, y))
-        img.put("#ffffff", (x, y+1))
-        img.put("#ffffff", (x-1, y))
-        img.put("#ffffff", (x, y-1))
-        img.put("#ffffff", (x+1, y+1))
-        img.put("#ffffff", (x-1, y-1))
-        img.put("#ffffff", (x+2, y+1))
-        img.put("#ffffff", (x+1, y+2))
-        img.put("#ffffff", (x-2, y-1))
-        img.put("#ffffff", (x-1, y-1))
-        img.put("#ffffff", (x-2, y-2))
-    elif x > 2 and y > 2:
-        img.put("#000000", (x, y))
-        img.put("#000000", (x+1, y))
-        img.put("#000000", (x, y+1))
-        img.put("#000000", (x-1, y))
-        img.put("#000000", (x, y-1))
-        img.put("#000000", (x+1, y+1))
-        img.put("#000000", (x-1, y-1))
-        img.put("#000000", (x+2, y+1))
-        img.put("#000000", (x+1, y+2))
-        img.put("#000000", (x-2, y-1))
-        img.put("#000000", (x-1, y-1))
-        img.put("#000000", (x-2, y-2))
     
 def task():
     while 1:
         for current in bodies:
             for each in bodies:
                 if current != each:
-                    if distance(current, each) < 2:
+                    if distance(current, each) < 10:
                         prevMass = current.mass
                         current.mass = current.mass + each.mass
                         current.velocity = ((current.velocity[0]*prevMass + each.velocity[0]*each.mass)/current.mass, (current.velocity[1]*prevMass + each.velocity[1]*each.mass)/current.mass)
+                        pygame.draw.circle(window, white, roundTuple(each.position), 2, 0)
                         bodies.remove(each)
                     else:
                         current.velocity = ((current.velocity[0] + gravityVector(current, each)[0]/current.mass), (current.velocity[1] + gravityVector(current, each)[1]/current.mass))
          
         for each in bodies:
-            paint_at(roundTuple(each.position), 0)
-        window.update()
+            #paint_at(roundTuple(each.position), 0)
+            pygame.draw.circle(window,white,roundTuple(each.position), 2, 0)
+#        pygame.display.flip()
         
         for each in bodies:
             each.position = ((each.position[0] + each.velocity[0]),(each.position[1] + each.velocity[1]))
         for each in bodies:
-            paint_at(roundTuple(each.position), 1)
-        window.update()
-        time.sleep(0.01)
+            #paint_at(roundTuple(each.position), 1)
+            pygame.draw.circle(window, black, roundTuple(each.position), 2, 0)
+        pygame.display.flip()
+        #time.sleep(0.01)
 
 
 
-p3 = Body((0, 2.583), (612, 386), .00001)
+p3 = Body((0, 2.583), (612, 386), .001)
 p4 = Body((0, 0), (512, 386), 100)
-p7 = Body((0, 2.11), (662, 386), .00001)
-p5 = Body((0, 1.826), (712, 386), .00001)
+p7 = Body((0, 2.11), (662, 386), .001)
+p5 = Body((0, 1.826), (712, 386), .001)
 p6 = Body ((0,1.29), (912, 386), 1)
-p8 = Body((0, 5.774946), (532, 386), .00001)
-moon1 = Body((0, 1.867494589), (932, 386), .0000000001)
+p8 = Body((0, 5.774946), (532, 386), .001)
+bhole = Body((-10, 0), (5000, 586), 500);
+#asteroid = Body((0, 3), (562, 386), .0000001);
+moon1 = Body((0, 1.867494589), (932, 386), .00001)
 
-bodies.append(p3)
-bodies.append(p4)
-bodies.append(p5)
-bodies.append(p6)
-bodies.append(p7)
-bodies.append(p8)
-bodies.append(moon1)
+#bodies.append(p3)
+#bodies.append(p4)
+#bodies.append(p5)
+#bodies.append(p6)
+#bodies.append(p7)
+#bodies.append(p8)
+#bodies.append(moon1)
+#bodies.append(bhole)
+#bodies.append(asteroid);
 
-#for i in range(0, 100):
-#    newBody = Body((0,0), (random.randint(0,1920), random.randint(0,768)), random.randint(1, 50))
-#    bodies.append(newBody)
+for i in range(0, 250):
+    newBody = Body((0,0), (random.randint(0,640), random.randint(0,480)), random.randint(1, 10))
+    bodies.append(newBody)
 
 task()
